c1cf1aec69963c5d3ee9ad3bc87406c56a2a1d9f
Sasha <sasha.chernigin@mail.ru>
2021-12-17 13:01:40 +0300

    Add Pinia

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+import {useUserStore} from '@/store/user';
+
+const store = useUserStore();
+
+store.fetchProfile()
 </script>
 
 <template>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <p>{{ store.profileData?.email ?? '' }}</p>
 </template>
 
 <style>
