663abc40ee6cc4192b18e0a25026fb6cd8d5c4ee
Sasha <sasha.chernigin@mail.ru>
2021-12-18 14:34:30 +0300

    Add Router

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
+import router from '@/router'
 import { createPinia } from 'pinia';
 import i18n from '@/plugins/i18n';
-import App from './App.vue'
+import App from '@/App.vue'
 
 createApp(App)
+    .use(router)
     .use(createPinia())
     .use(i18n)
     .mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import routes from '~pages'
+
+import NotFoundComponent from '@/components/NotFound.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        ...routes,
+        { path: '/:pathMatch(.*)', component: NotFoundComponent }
+    ],
+});
+
+export default router
