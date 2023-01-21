dc9d1c9b1d928adc82aafd461b849b3503bee455
Sasha <sasha.chernigin@mail.ru>
2023-01-21 10:55:12 +0300

    Add stub to the Trainings page

--- src/pages/trainings/index.vue
+++ src/pages/trainings/index.vue
@@ -5,9 +5,29 @@ meta:
 </route>
 
 <script setup lang="ts">
+import { BuildOutline } from '@vicons/ionicons5'
 </script>
 
 <template>
-    <div></div>
+    <n-row
+        class="trainings-stub"
+        justify-content="center"
+        align-items="center">
+        <n-result
+            status="info"
+            :title="$t('coming_soon')"
+            size="huge"
+        >
+            <template #icon>
+                <n-icon size="80">
+                    <BuildOutline />
+                </n-icon>
+            </template>
+        </n-result>
+    </n-row>
 </template>
+
+<style scoped lang="scss">
+@import "@/pages/styles/trainings";
+</style>
 
