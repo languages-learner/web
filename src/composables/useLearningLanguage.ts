dd18546a006348861a353679c46e8511bf6c8515
Aleksandr Chernigin <sasha.chernigin@mail.ru>
2024-02-06 17:26:24 +0400

    Add learning language to settings

---
+++ src/composables/useLearningLanguage.ts
@@ -0,0 +1,29 @@
+import { useUserStore } from '@/store/modules/user'
+import { BASE_INTERFACE_LANGUAGE } from '@/const/BaseInterfaceLanguage'
+import { useConfigStore } from '@/store/modules/config'
+
+export const useLearningLanguage = () => {
+    const { customData } = storeToRefs(useUserStore())
+    const { getTranslatedLanguageName, languagesAvailableForLearning } = useConfigStore()
+    const { updateActiveLearningLanguage } = useUserStore()
+
+    const activeLearningLanguage = computed(() => unref(customData)?.activeLearningLanguage)
+    const activeLearningLanguageName = computed(() => getTranslatedLanguageName(unref(activeLearningLanguage) ?? BASE_INTERFACE_LANGUAGE))
+    const availableLearningLanguagesOptions = computed(() =>
+        languagesAvailableForLearning
+            .map(languageId => ({
+                key: languageId,
+                label: getTranslatedLanguageName(languageId),
+            })))
+    const availableLearningLanguagesOptionsExceptActive = computed(() =>
+        unref(availableLearningLanguagesOptions).filter(({ key }) => key !== unref(customData)?.activeLearningLanguage),
+    )
+
+    return {
+        activeLearningLanguage,
+        activeLearningLanguageName,
+        availableLearningLanguagesOptions,
+        availableLearningLanguagesOptionsExceptActive,
+        updateActiveLearningLanguage,
+    }
+}
