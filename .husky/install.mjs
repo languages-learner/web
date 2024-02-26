45756e022a9a12a4557260658f5fe2c6ef7cf905
Aleksandr Chernigin <sasha.chernigin@mail.ru>
2024-02-26 16:26:09 +0400

    Cache node_modules in ci

---
+++ .husky/install.mjs
@@ -0,0 +1,7 @@
+// https://typicode.github.io/husky/how-to.html
+// Skip Husky install in production and CI
+if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+    process.exit(0)
+}
+const husky = (await import('husky')).default
+console.log(husky())
